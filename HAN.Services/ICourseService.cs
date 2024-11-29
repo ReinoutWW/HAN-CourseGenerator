e1bcce5294be4aa7039d2110cc5230d916787640
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-11-29 17:47:30 +0100

    Create ICourseService.cs

---
+++ HAN.Services/ICourseService.cs
@@ -0,0 +1,16 @@
+﻿using HAN.Domain.Entities;
+using HAN.Domain.Entities.CourseComponents;
+
+namespace HAN.Services;
+
+public interface ICourseService
+{
+    // Create
+    Course CreateCourse(Course course);
+    EVL CreateEVL(EVL evl);
+    CourseComponent CreateCourseComponent(CourseComponent component);
+    
+    // Assign
+    void AddEVLToCourse(int courseId, int evlId);
+    void AddCourseComponentToCourse(int courseId, int componentId);
+}
