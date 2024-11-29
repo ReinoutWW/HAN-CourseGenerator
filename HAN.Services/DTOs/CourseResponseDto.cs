4a87da98d39eb478b1148f144ee78f684f6a9386
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-11-29 22:05:55 +0100

    Add DTO's

---
+++ HAN.Services/DTOs/CourseResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HAN.Services.DTOs;
+
+public class CourseResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ HAN.Services/DTOs/CreateCourseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace HAN.Services.DTOs;
+
+public class CreateCourseDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
