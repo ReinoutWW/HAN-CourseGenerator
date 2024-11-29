437a96efe7f2380aff413d2268c2046ef460281e
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-11-29 22:06:47 +0100

    Add CourseTests (Initial)

---
+++ HAN.Tests/CourseServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using HAN.Services;
+using HAN.Services.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HAN.Tests;
+
+public class CourseServiceTests : TestBase
+{
+    private readonly ICourseService _courseService;
+    
+    public CourseServiceTests()
+    {
+        _courseService = ServiceProvider.GetRequiredService<ICourseService>();
+    }
+
+    [Fact]
+    public void CreateCourse_ShouldCreateCourse()
+    {
+        CreateCourseDto course = new()
+        {
+            Name = "Test Course",
+            Description = "Test Description",
+        };
+
+        var createdCourse = _courseService.CreateCourse(course);
+        
+        Assert.NotNull(createdCourse);
+        Assert.Equal(course.Name, createdCourse.Name);
+        Assert.Equal(course.Description, createdCourse.Description);
+    }
+}
