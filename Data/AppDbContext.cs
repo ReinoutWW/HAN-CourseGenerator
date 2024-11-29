92bb0acde86484a213babcc57e3e59c78a6a3154
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-11-29 16:14:51 +0100

    Add AppDbContext annotation validation

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using HAN.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using HAN.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace HAN.Data;
@@ -8,4 +9,31 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    
+    public override int SaveChanges()
+    {
+        ValidateEntities();
+        return base.SaveChanges();
+    }
+    
+    private void ValidateEntities()
+    {
+        var entities = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified)
+            .Select(e => e.Entity);
+
+        foreach (var entity in entities)
+        {
+            var validationResults = new List<ValidationResult>();
+            var context = new ValidationContext(entity);
+
+            if (!Validator.TryValidateObject(entity, context, validationResults, true))
+            {
+                foreach (var validationResult in validationResults)
+                {
+                    throw new ValidationException(validationResult.ErrorMessage);
+                }
+            }
+        }
+    }
 }
