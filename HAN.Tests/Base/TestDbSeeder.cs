f0b1bf3fc2ceb662db192c8e8d846f55272d4490
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-14 16:59:22 +0100

    Remove unused code - SonarLint

--- HAN.Tests/Base/TestDbSeeder.cs
+++ HAN.Tests/Base/TestDbSeeder.cs
@@ -8,21 +8,6 @@ namespace HAN.Tests.Base;
 
 public static class TestDbSeeder
 {
-    public static void SeedUsers(AppDbContext context, int seedUserCount, string userPrefix)
-    {
-        var users = Enumerable.Range(0, seedUserCount)
-            .Select(i => {
-                var user = DbEntityCreator<User>.CreateEntity();
-                user.Name = $"{userPrefix}{i + 1}";
-                user.Email = $"{userPrefix}{i + 1}@coursegenerator.com";
-                return user;
-            })
-            .ToList();
-
-        context.Users.AddRange(users);
-        context.SaveChanges();
-    }
-
     public static void SeedCourses(AppDbContext context, int seedCourseCount)
     {
         var courses = Enumerable.Range(0, seedCourseCount)
@@ -43,37 +28,6 @@ public static class TestDbSeeder
         context.SaveChanges();
 
         return evls;
-    }
-    
-    private static List<Evl> SeedEvlsWithCourseComponents(AppDbContext context, int seedEvlCount)
-    {
-        var evls = SeedEvls(context, seedEvlCount);
-        
-        evls.SeedCourseComponentsForEvls(context);
-
-        context.SaveChanges();
-
-        return evls;
-    }
-    
-
-    private static void SeedCourseComponentsForEvls(this List<Evl> evls, AppDbContext context)
-    {
-        evls.ForEach(evl =>
-        {
-            var courseComponent = DbEntityCreator<CourseComponent>.CreateEntity();
-
-            context.CourseComponents.Add(courseComponent);
-            context.SaveChanges();
-
-            courseComponent = context.CourseComponents
-                .Include(c => c.Evls)
-                .Single(c => c.Id == courseComponent.Id);
-
-            courseComponent.Evls.Add(evl);
-        });
-        
-        context.SaveChanges();
     }
     
     public static void SeedCourseComponents(AppDbContext context, int seedCourseComponentCount)
