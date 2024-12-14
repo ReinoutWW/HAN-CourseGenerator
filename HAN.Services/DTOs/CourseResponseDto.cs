c7a29bb3db2e13c24e131128ae4f7404c75e9824
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-14 16:57:43 +0100

    Add CourseValidationService

--- HAN.Services/DTOs/CourseResponseDto.cs
+++ HAN.Services/DTOs/CourseResponseDto.cs
@@ -1,8 +1,11 @@
-﻿namespace HAN.Services.DTOs;
+﻿using HAN.Domain.Entities;
+
+namespace HAN.Services.DTOs;
 
 public class CourseResponseDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public IEnumerable<Evl> Evls { get; set; } = new List<Evl>();
 }
