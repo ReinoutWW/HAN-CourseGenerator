344050031b9da31761b2c8c2f707d95d9a61bd28
Wob Jelsma <wobjelsm@gmail.com>
2024-12-18 20:07:19 +0100

    refactor: Evl validation rules to dto

--- Data/Entities/Evl.cs
+++ Data/Entities/Evl.cs
@@ -6,15 +6,12 @@ namespace HAN.Data.Entities;
 
 public class Evl : BaseEntity
 {
-    
-    [Required(ErrorMessage = "Name is required.")] 
-    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")] 
-    [MinLength(2, ErrorMessage = "Name should be at least 5 characters.")]
+    [Required]
+    [DataType(DataType.Text)]
     public string Name { get; set; } = string.Empty;
     
-    [Required(ErrorMessage = "Description is required.")] 
-    [StringLength(450, ErrorMessage = "Description cannot exceed 450 characters.")] 
-    [MinLength(5, ErrorMessage = "Description should be at least 5 characters.")]
+    [Required]
+    [DataType(DataType.Text)]
     public string? Description { get; set; }
     
     [Required]
@@ -25,7 +22,4 @@ public class Evl : BaseEntity
     [Required]
     [DataType(DataType.DateTime)]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    
-    public ICollection<Lesson>? Lessons { get; set; } = new List<Lesson>();
-    public ICollection<Exam>? Exams { get; set; } = new List<Exam>();
 }
--- HAN.Services/DTOs/CreateEvlDto.cs
+++ HAN.Services/DTOs/CreateEvlDto.cs
@@ -4,7 +4,13 @@ namespace HAN.Services.DTOs;
 
 public class CreateEvlDto
 {
+    [Required(ErrorMessage = "Name is required.")] 
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")] 
+    [MinLength(2, ErrorMessage = "Name should be at least 5 characters.")]
     public string Name { get; set; } = string.Empty;
     
+    [Required(ErrorMessage = "Description is required.")] 
+    [StringLength(450, ErrorMessage = "Description cannot exceed 450 characters.")] 
+    [MinLength(5, ErrorMessage = "Description should be at least 5 characters.")]
     public string? Description { get; set; }
 }
