d7e76b07453ed0acf427cf3fc82144c1a6eac622
Wob Jelsma <wobjelsm@gmail.com>
2024-12-18 20:04:23 +0100

    refactor: CourseComponent validation rules to dto

--- HAN.Services/DTOs/CreateCourseComponentDto.cs
+++ HAN.Services/DTOs/CreateCourseComponentDto.cs
@@ -4,7 +4,11 @@ namespace HAN.Services.DTOs;
 
 public class CreateCourseComponentDto
 {
+    [Required(ErrorMessage = "Name is required.")] 
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")] 
+    [MinLength(5, ErrorMessage = "Name cannot exceed 5 characters.")]
     public string Name { get; set; } = string.Empty;
     
+    [StringLength(450, ErrorMessage = "Description cannot exceed 450 characters.")] 
     public string? Description { get; set; }
 }
