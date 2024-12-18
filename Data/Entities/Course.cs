6279180d97b793fd73ab935b6cafbb58968836f5
Wob Jelsma <wobjelsm@gmail.com>
2024-12-18 20:03:34 +0100

    refactor: validation rules to dto

--- Data/Entities/Course.cs
+++ Data/Entities/Course.cs
@@ -5,13 +5,10 @@ namespace HAN.Data.Entities;
 
 public class Course : BaseEntity
 {
-    [Required(ErrorMessage = "Name is required.")] 
-    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")] 
-    [MinLength(5, ErrorMessage = "Name cannot exceed 5 characters.")]
+    [Required]
     [DataType(DataType.Text)]
     public string Name { get; set; } = string.Empty;
     
-    [StringLength(450, ErrorMessage = "Description cannot exceed 450 characters.")] 
     [DataType(DataType.Text)]
     public string? Description { get; set; }
     
