2e637cafd522f5fb0538105969e16e84413e874a
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-20 14:48:19 +0100

    Add CourseComponentService

--- HAN.Services/Interfaces/ICourseComponentService.cs
+++ HAN.Services/Interfaces/ICourseComponentService.cs
@@ -1,12 +1,14 @@
 ﻿using HAN.Services.DTOs;
 using HAN.Services.DTOs.CourseComponents;
 
-namespace HAN.Services;
+namespace HAN.Services.Interfaces;
 
 public interface ICourseComponentService
 {
-    LessonDto CreateLesson(LessonDto lesson);
-    
-    
-    void AddEvlToCourseComponent(int courseComponentId, int evlId);
+    public T CreateCourseComponent<T>(T courseComponentDto) where T : CourseComponentDto;
+    public void AddEvlToCourseComponent(int courseComponentId, int evlId);
+    public void AddFileToCourseComponent(int courseComponentId, int fileId);
+    public List<FileDto> GetFilesForCourseComponent(int courseComponentId);
+    public List<EvlDto> GetEvlsForCourseComponent(int courseComponentId);
+    public T GetCourseComponentById<T>(int id) where T : CourseComponentDto;
 }
---
+++ HAN.Services/Mappers/CourseComponentTypeMap.cs
@@ -0,0 +1,23 @@
+﻿using HAN.Data.Entities.CourseComponents;
+using HAN.Services.DTOs.CourseComponents;
+
+namespace HAN.Services.Mappers;
+
+public static class CourseComponentTypeMap
+{
+    private static readonly Dictionary<Type, Type> _dtoToEntityMap = new()
+    {
+        { typeof(LessonDto), typeof(Lesson) },
+        { typeof(Lesson), typeof(LessonDto) }
+    };
+
+    public static Type GetEntityType(Type dtoType)
+    {
+        if (_dtoToEntityMap.TryGetValue(dtoType, out var entityType))
+        {
+            return entityType;
+        }
+
+        throw new InvalidOperationException($"No entity type mapping found for DTO type {dtoType.FullName}");
+    }
+}
