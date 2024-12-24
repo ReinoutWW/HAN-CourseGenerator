fb526c61a1352b486002b878a0343b53b8c6fa70
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-24 17:02:41 +0100

    Multiple courses test data

--- HAN.Client/Data/DataSeeder.cs
+++ HAN.Client/Data/DataSeeder.cs
@@ -7,30 +7,88 @@ public static class DataSeeder
 {
     public static void SeedCourseData(IServiceProvider serviceProvider)
     {
+        int iterations = 10;
+        
+        for (int i = 0; i < iterations; i++)
+        {
+            SeedCourseIteration(i, serviceProvider);
+        }
+    }
+
+    private static void SeedCourseIteration(int iteration, IServiceProvider serviceProvider)
+    {
         var evlService = serviceProvider.GetRequiredService<IEvlService>();
         var courseService = serviceProvider.GetRequiredService<ICourseService>();
 
         var evlDto1 = new EvlDto()
         {
-            Name = "OOP",
+            Name = $"OOP {iteration}",
             Description = "Object Oriented Programming",
         };
         var evlDto2 = new EvlDto()
         {
-            Name = "Databases",
+            Name = $"Databases {iteration}",
             Description = "Database Management",
         };
 
         evlDto1 = evlService.CreateEvl(evlDto1);
         evlDto2 = evlService.CreateEvl(evlDto2);
-
+    
+        var courseName =  $"{GetRandomCourseName()} {iteration}";
+        
         var course = new CourseDto()
         {
-            Name = "I-OOSE",
-            Description = "Introduction to Object Oriented Software Engineering",
+            Name = courseName,
+            Description = $"Introduction to {courseName}. After this course, you will be able to understand the basics of {courseName}.",
             Evls = [evlDto1, evlDto2]
         };
 
         courseService.CreateCourse(course);
+    }
+
+    private static string GetRandomCourseName()
+    {
+        var courseNames = new []
+        {
+            "I-OOSE",
+            "Programming",
+            "UX Design",
+            "Web Development",
+            "Requirements Engineering",
+            "Software Testing",
+            "Software Architecture",
+            "Software Project Management",
+            "Software Quality Assurance",
+            "Software Development",
+            "Software Engineering",
+            "Software Maintenance",
+            "Software Design",
+            "Software Analysis",
+            "Software Development Methodologies",
+            "Software Development Processes",
+            "Software Development Tools",
+            "Software Development Techniques",
+            "Software Development Principles",
+            "Software Development Patterns",
+            "Software Development Practices",
+            "Software Development Strategies",
+            "Software Development Concepts",
+            "Software Development Technologies",
+            "Software Development Frameworks",
+            "Software Development Languages",
+            "Software Development Paradigms",
+            "Software Development Environments",
+            "Software Development Platforms",
+            "Software Development Models",
+            "Software Development Approaches",
+            "Software Development Strategies",
+            "Software Development Techniques",
+            "Software Development Tools",
+            "Software Development Practices",
+            "Software Development Patterns",
+        };
+        
+        var randomIndex = new Random().Next(0, courseNames.Length);
+        return courseNames[randomIndex];
     }
 }
