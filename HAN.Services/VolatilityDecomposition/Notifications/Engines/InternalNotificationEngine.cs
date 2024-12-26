573128099da34a9421f1df83150eb2935191fa1a
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-26 14:49:12 +0100

    Notification Engines | Email + Internal

---
+++ HAN.Services/VolatilityDecomposition/Notifications/Engines/InternalNotificationEngine.cs
@@ -0,0 +1,39 @@
+﻿namespace HAN.Services.VolatilityDecomposition.Notifications.Engines;
+
+public class InternalNotificationEngine : INotificationEngine
+{
+    private readonly InternalNotificationStateService _internalNotificationStateService;
+
+    public InternalNotificationEngine(InternalNotificationStateService internalNotificationStateService)
+    {
+        _internalNotificationStateService = internalNotificationStateService;
+    }
+
+    public NotificationMethod Method => NotificationMethod.Internal;
+
+    public void Notify(Notification notification)
+    {
+        switch (notification)
+        {
+            case EntityPersistedNotification entityNotification:
+                HandleEntityPersistedNotification(entityNotification);
+                break;
+
+            default:
+                HandleGenericNotification(notification);
+                break;
+        }
+    }
+
+    private void HandleEntityPersistedNotification(EntityPersistedNotification notification)
+    {
+        _internalNotificationStateService.AddNotification(notification);
+        Console.WriteLine($"Internal Notification: {notification.PersistData.EntityName} created successfully!");
+    }
+
+    private void HandleGenericNotification(Notification notification)
+    {
+        _internalNotificationStateService.AddNotification(notification);
+        Console.WriteLine($"Internal Notification: {notification.Title} - {notification.Message}");
+    }
+}
