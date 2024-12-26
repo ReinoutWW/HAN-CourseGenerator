325932f6b2e5cf27e7d2f25b2c5295dd8e6654a1
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-26 14:49:30 +0100

    Temp hard-coded UserPreferences for methods

---
+++ HAN.Services/VolatilityDecomposition/UserPreferenceFactory.cs
@@ -0,0 +1,31 @@
+﻿using HAN.Services.VolatilityDecomposition.Notifications;
+
+namespace HAN.Services.VolatilityDecomposition;
+
+public class UserPreferenceFactory
+{
+    public IUserPreferences CreatePreferences()
+    {
+        return new HardCodedUserPreferences();
+    }
+}
+
+// For the experiment, we will use a hard-coded user preference class
+public class HardCodedUserPreferences : IUserPreferences
+{
+    public Dictionary<NotificationType, List<NotificationMethod>> Preferences { get; }
+
+    public HardCodedUserPreferences()
+    {
+        Preferences = new Dictionary<NotificationType, List<NotificationMethod>>
+        {
+            {
+                NotificationType.EntityPersisted, new List<NotificationMethod>
+                {
+                    NotificationMethod.Internal,
+                    NotificationMethod.Email
+                }
+            }
+        };
+    }
+}
