ffbc17ec02ce65defbff455a4258f09c4b080497
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-12-27 12:05:50 +0100

    Introduce Primary card + flat style

---
+++ HAN.Client/Components/UI/PrimaryCard.cs
@@ -0,0 +1,13 @@
+﻿using Radzen;
+using Radzen.Blazor;
+
+namespace HAN.Client.Components.UI;
+
+public class PrimaryCard : RadzenCard
+{
+    protected override void OnInitialized()
+    {
+        Variant = Variant.Flat;
+        base.OnInitialized();
+    }
+}
