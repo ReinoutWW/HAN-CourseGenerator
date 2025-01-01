0053c38cdd4866c8acad8bd8092482bbd0f43331
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2025-01-01 17:57:20 +0100

    DI Injections for Adapters

--- HAN.Client/Program.cs
+++ HAN.Client/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HAN.Client;
 using HAN.Client.Auth;
 using HAN.Client.Components.Base;
+using HAN.Client.Services;
+using HAN.Services.Dummy;
 using Microsoft.AspNetCore.Components.Authorization;
 using Radzen;
 
@@ -13,13 +15,26 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddRadzenComponents();
 builder.Services.AddScoped<SystemFeedbackNotificationService>();
+
+builder.Services.AddScoped<CourseService>();
+
 builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new Uri("https://localhost:5005"));
+builder.Services.AddScoped<ICourseApiAdapter, CourseApiAdapter>(sp =>
+{
+    var httpClient = sp.GetRequiredService<HttpClient>();
+    var baseUri = sp.GetRequiredService<Uri>();
+    return new CourseApiAdapter(httpClient, baseUri);
+});
 
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
 
 await app.RunAsync();
