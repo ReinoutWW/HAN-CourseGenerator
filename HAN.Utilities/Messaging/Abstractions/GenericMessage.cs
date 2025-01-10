28961cde7f96511bb2bcf12831292ab99e0b2fb3
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2025-01-10 21:34:41 +0100

    Create GenericMessage.cs

---
+++ HAN.Utilities/Messaging/Abstractions/GenericMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace HAN.Utilities.Messaging.Abstractions;
+
+public class GenericMessage : IMessage
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Action { get; set; }
+    public string Payload { get; set; }
+}
